6f56f2f6dd877a2d9fae21232c6586d7e204faa1
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2023-04-01 15:51:06 +0200

    added new chat model, create database object, changed Model fields

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from chat.models import Message
+from chat.models import Chat
+
+class Messageadmin(admin.ModelAdmin):
+    fields = ('chat','text','created_at','author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
+    
+
 # Register your models here.
+
+admin.site.register(Message, Messageadmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from chat.models import Message
+from chat.models import Chat
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text = request.POST['textmessage'], chat = myChat, author= request.user, receiver= request.user)
+    return render(request, 'chat/index.html', {'username': 'Alex'})
